1359c5cc11c183390031d7b5352f134c9023b4f4
mashum22 <jotajombang@gmail.com>
2024-04-04 11:04:36 +0700

    lanjutan

---
+++ app/Views/books/Detail.php
@@ -0,0 +1,13 @@
+<?= $this->extend('layout/template');?>
+
+<?= $this->section('content');?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h2 class="mt-2">Detai buku</h2>
+            <div class="card mb-3"> 
+                <style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4>
+                    <img src="/img/<?=$buku[]
+
